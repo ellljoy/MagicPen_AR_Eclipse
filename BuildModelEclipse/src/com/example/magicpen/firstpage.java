c8cca22fc08ad488025ba7a13b2dcbe8b2ab131e
Gavin YAO <ellljoy.iot@gmail.com>
2016-03-21 17:43:59 +0100

    Move rendering activity from BuildModel.class into Firstpage.class

--- BuildModelEclipse/src/com/example/magicpen/firstpage.java
+++ BuildModelEclipse/src/com/example/magicpen/firstpage.java
@@ -5,6 +5,7 @@ import com.example.magicpen.R;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 
@@ -17,8 +18,9 @@ import min3d.vos.Light;
 public class firstpage extends RendererActivity {
 
     private ImageView btn_back;
-    private ImageView btn_weather;
     private ImageView btn_instruction;
+    private Button btn1;
+    private Button btn2;
 	private Object3dContainer objModel;
 
     @Override public void onCreate(Bundle savedInstanceState) {
@@ -26,25 +28,38 @@ public class firstpage extends RendererActivity {
     	/** Called when the activity is first created. */
         super.onCreate(savedInstanceState);
         setContentView(R.layout.firstpage);
+        
+        //TBD Click btn1 to build model#
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+            
+        });
+        
+        //TBD Click btn2 to build model#2
+        btn2 = (Button) findViewById(R.id.btn2);
  
+        //Click btn Back to go back
         btn_back = (ImageView) findViewById(R.id.btnback);
         btn_back.setOnClickListener(new View.OnClickListener() {
 
-            @Override
+        	@Override
             public void onClick(View v) {
-// Switch to PageB
                 Intent intent = new Intent(firstpage.this, MainActivity.class);
                 startActivity(intent);
                 firstpage.this.finish();
             }
         });
 
+        //Click btn Instruction for guides
         btn_instruction = (ImageView) findViewById(R.id.btninstruction);
         btn_instruction.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-// Switch to PageB
                 Intent intent = new Intent(firstpage.this, SwiptPages.class);
                 startActivity(intent);
                 firstpage.this.finish();
@@ -68,7 +83,6 @@ public class firstpage extends RendererActivity {
 		objModel = parser.getParsedObject();
 		objModel.position().x = 0f; 
 		objModel.position().y = 0f;
-		objModel.position().z = -5f;
 		objModel.scale().x = objModel.scale().y = objModel.scale().z = .5f;
 		scene.camera().target = objModel.position();
 		scene.addChild(objModel);
@@ -78,6 +92,5 @@ public class firstpage extends RendererActivity {
 	@Override
 	public void updateScene() {
 		objModel.rotation().x++;
-		objModel.rotation().z++;
 	}
 }
